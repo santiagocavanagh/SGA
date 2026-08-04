531b203c61d45e5464383a5e1187689d5c571290
santiagocavanagh <santicav92@gmail.com>
2026-08-04 15:45:46 -0300

    primero ejercicio

---
+++ JS/excersice1.js
@@ -0,0 +1,8 @@
+console.log("Solicitando lista de Alumnos")
+setTimeout(() => {
+    console.log("Programa Ejecutandose")
+}, 2000,
+setTimeout(() => {
+    console.log("Lista Recibida")
+}, 5000)
+);
