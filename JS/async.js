e012159a2394f6f8043fb8b06a1b68fd2e58a037
santiagocavanagh <santicav92@gmail.com>
2026-08-04 14:58:03 -0300

    clase 02 - SetTimeOut/Async

---
+++ JS/async.js
@@ -0,0 +1,43 @@
+/*console.log("Inicio")
+setTimeout(() => {
+    console.log("Buscando alumnos...")
+}, 3000)
+setTimeout(() => {
+    console.log("Fin")
+}, 5000)
+
+
+function saludar() {
+    console.log("Hola")
+}
+
+function ejecutar(funcion){
+    funcion();
+}
+ejecutar(saludar)
+
+function despedirse(){
+    console.log("Trabajo completado \nGracias por tu visita")
+}
+setTimeout(despedirse, 6000)
+
+setTimeout(() => {
+    console.log("buscando docentes...")
+}, 2000);
+
+setTimeout(() => {
+    console.log("buscando materias...")
+}, 3000);
+
+setTimeout(() => {
+    console.log("buscando alumnos...")
+}, 4000);
+*/
+
+console.log("Abirendo SGA")
+setTimeout(() => {
+    console.log("Alumnos Cargados...")
+}, 3000);
+
+console.log("El usuario Puede Seguir Navegando")
+
